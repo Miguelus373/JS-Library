31091f4b1dd9d87ac47e812c32d0934bf777a8c1
chinweokwu <paulcmorah@gmail.com>
2020-11-19 23:41:29 +0100

    added display fuction

--- scripts.js
+++ scripts.js
@@ -5,6 +5,8 @@ let a = document.getElementById("author");
 let p = document.getElementById("pages");
 let no = document.getElementById("unread");
 let yes = document.getElementById("read");
+let cardGroup = document.querySelectorAll(".card-group")
+cardGroup.style.color = "blue";
 
 function Book(title, author, pages, read) {
   this.title = title
@@ -42,5 +44,42 @@ function getInputValue() {
 function createBook() {
   if (validation()) {
     getInputValue();
+    display();
   }
+}
+
+function display() {
+  for(let i = 0; i < library.length; i++){
+      cards(library[i]);
+  }
+}
+
+function cards(card) {
+   let cardy = document.createElement("div");
+   cardy.classList.add("card");
+
+    let cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+   
+
+    let cardTitle  = document.createElement("h4");
+    cardTitle.textContent = card.title;
+    cardBody.appendChild(cardTitle);
+
+    let cardAuthor = document.createElement("p");
+    cardAuthor.textContent = card.author;
+    cardBody.appendChild(cardAuthor);
+
+    let cardPages  = document.createElement("p");
+    cardPages.textContent = card.pages;
+    cardBody.appendChild(cardPages);
+
+    let cardStatus  = document.createElement("p");
+    cardStatus.textContent = card.read;
+    cardBody.appendChild(cardStatus);
+
+
+     cardy.appendChild(cardBody);
+
+     cardGroup.appendChild(cardy)
 }
