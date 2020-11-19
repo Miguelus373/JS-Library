037c51ae75a3ab2b07dbb5aec314c0d133ed4cfb
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-11-19 12:57:20 -0400

    Add basic form and funtions

--- scripts.js
+++ scripts.js
@@ -1,4 +1,38 @@
-document.getElementById("myBtn").addEventListener("click", displayDate);
-function displayDate() {
-    document.getElementById("myBtn").innerHTML = Date();
+let library = [];
+
+function Book([title, author, pages, read]) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+}
+
+function validation(form) {
+  let inputValid = form;
+
+  if (form[0] == '' || form[1] == '' || form[2] == '') {
+    alert('Check your data')
+  } else {
+    library.push(form)
   }
+}
+
+function getInputValue() {
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let status;
+
+  if (document.getElementById("unread").checked) {
+    status = document.getElementById("unread").value;
+  } else {
+    status = document.getElementById("read").value;
+  }
+
+  let inputs = [title, author, pages, status];
+  return inputs
+}
+
+function onclickk() {
+  validation(getInputValue())
+}
